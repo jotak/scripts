3531584bcccff8efa723852f4abaca7929e8a80e
Joel Takvorian <jtakvori@redhat.com>
2016-11-07 10:24:16 +0100

    Add setgitaliases.sh

---
+++ setgitaliases.sh
@@ -0,0 +1,13 @@
+
+git config --global "user.name" "Joel Takvorian"
+git config --global "user.email" "joel.takvorian@qaraywa.net"
+git config --global "color.ui" "auto"
+git config --global "alias.br" "branch"
+git config --global "alias.co" "checkout"
+git config --global "alias.lg" "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global "alias.st" "status"
+git config --global "alias.rst" "reset --hard"
+git config --global "merge.tool" "meld"
+git config --global "mergetool.keepbackup" "false"
+
+
