6a310bdd61b7f368d427536e7ef2cac5ba2de5dd
Joel Takvorian <jtakvori@redhat.com>
2019-08-06 09:17:26 +0200

    improve copr

--- checkout-pr.sh
+++ checkout-pr.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [[ "$#" -lt 1 || "$1" = "--help" ]]; then
-	echo "Syntax: copr <url> [<remote>]"
+	echo "Syntax: copr <PR URL or ID> [<remote>]"
 	echo ""
 	echo "Fetch and checkout remote pull-request from github"
 	echo "  ex : copr https://github.com/jotak/scripts/pull/2 origin"
+	echo "  ex : copr 2 upstream"
 	echo ""
 	echo "When not provided, <remote> defaults to 'upstream'"
 	echo "  ex : copr https://github.com/hawkular/hawkular-services/pull/220"
@@ -23,7 +24,16 @@ URL=( $1 )
 IFS='/'
 PARTS=( ${URL[0]} )
 
-ID="${PARTS[6]}"
+if [[ "${#PARTS[@]}" == "1" ]]; then
+	ID="${PARTS[0]}"
+elif [[ "${#PARTS[@]}" -ge "7" ]]; then
+	ID="${PARTS[6]}"
+else
+	echo "Could not read parameter $1: URL or ID expected"
+	exit
+fi
+
+echo "Pull request ID: $ID"
 BRANCHNAME="pr-$ID"
 
 git fetch $REMOTE pull/$ID/head:$BRANCHNAME
